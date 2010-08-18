c51742b5601f7e7f8203e4a521c04e044846f9cd
Stefano B <stefano.ballabeni+github@gmail.com>
2010-08-18 19:52:49 -0400

    can specify the keywords to hl (default: username)

--- highlight.js
+++ highlight.js
@@ -12,6 +12,40 @@
             else style.appendChild(rules);
             head.appendChild(style);  
         },
+        add_default_highlight = function () {
+            if (chat) {
+                $('words-to-highlight').value = chat.username;
+                set_highlight_words();
+            }
+        },
+        set_highlight_words = function () {
+          highlight_words = $('words-to-highlight').value.split(' ');
+          set_regexp();
+        },
+        highlight_config = function () {
+            var html = '<div class="participants">\
+              <div>\
+                <h3>Highlight (cfpp)</h3>\
+              </div>\
+              <div id="participants">\
+                <ul class="participant-list">\
+                    <li class="user nubbin_region">\
+                        <span class="name">Hightlight words</span>\
+                    </li>\
+                    <li class="user nubbin_region">\
+                        <input type="text" id="words-to-highlight" />\
+                    </li>\
+              </ul>\
+              </div>\
+            </div>';
+            
+            $('participants').insert({"after": html});
+            add_default_highlight();
+
+            $('words-to-highlight').observe('blur', function (e) {
+                set_highlight_words();
+            });
+        },
         highlight = function(message) {
             var txt = message.bodyCell.children[0].innerHTML;
             message.bodyCell.children[0].innerHTML = txt.replace(regexp, "<span class=\"cfpp-hl\">$1</span>");
@@ -22,14 +56,15 @@
                 highlight(message);
             })
         },
-        highlight_words = [
-            'midu',
-            'stefano',
-            'ballabeni'
-        ],
-        regexp = new RegExp("(" + highlight_words.join('|') + ")", "gim");
+        set_regexp = function () {
+            regexp = new RegExp("(" + highlight_words.join('|') + ")", "gim");
+        },
+        highlight_words = [],
+        regexp = null;
 
+    set_regexp();
     add_styles();
+    highlight_config();
 
     if (typeof chat.events.messagesInserted != "undefined") {
         chat.events.messagesInserted.push(messages_inserted);
